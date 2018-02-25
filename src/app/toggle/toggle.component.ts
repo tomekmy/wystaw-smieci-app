8cee9c14796afeeeeaabc99fb0b4c4eee24fa58a
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2018-02-25 20:20:23 +0100

    Add toggle sector indicator

--- src/app/toggle/toggle.component.ts
+++ src/app/toggle/toggle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-toggle',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toggle.component.scss']
 })
 export class ToggleComponent implements OnInit {
+  sector = 'Wszystkie';
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.sectorUpdated.subscribe((id: number) => {
+      this.sector = this.dataService.getSectors()[id].viewValue;
+    });
   }
 
 }
