886934449df95649548a58873d9f99725021cc2e
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2018-02-26 17:58:57 +0100

    On reload set recently selected sector description

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -15,6 +15,7 @@ export class DescriptionComponent implements OnInit {
   type: string;
   initialDates = [];
   sortedDates = [];
+  sectors = this.dataService.getSectors();
 
   constructor(private dataService: DataService) { }
 
@@ -22,9 +23,9 @@ export class DescriptionComponent implements OnInit {
     // Get locale from data service
     this.locale = this.dataService.getLocale();
 
-    // Get selected sectors description
-    this.descriptionTitle = this.dataService.getSectors()[0].name;
-    this.descriptionBoundary = this.dataService.getSectors()[0].boundary;
+    // By default set descriptions to "Wszystkie" sector
+    this.descriptionTitle = this.sectors[0].name;
+    this.descriptionBoundary = this.sectors[0].boundary;
 
     // Default sorted dates
     this.dataService.getJSON().subscribe(data => {
@@ -32,12 +33,40 @@ export class DescriptionComponent implements OnInit {
       this.sortedDates = this.initialDates.filter(dates => +dates.term > Date.now()).sort((a, b) =>  a.term - b.term);
       this.nextDate = this.sortedDates[0].term;
       this.type = this.sortedDates[0].type;
+
+      // Get selected sectors description
+      // Check if user select any sector before
+      if (localStorage.lastSelectedSector) {
+        for (const key of Object.keys(this.sectors)) {
+          if (this.sectors[key].value === localStorage.lastSelectedSector) {
+            this.descriptionTitle = this.sectors[key].name;
+            this.descriptionBoundary = this.sectors[key].boundary;
+
+            // Get nearest garbage collection date
+            if (key === '0') {
+              this.sortedDates = this.initialDates.filter(dates => +dates.term > Date.now());
+            } else {
+              this.sortedDates = this.initialDates.filter(dates => {
+                if (dates.sector === 'yellow') {
+                  dates.sector = 'gold';
+                }
+                return +dates.term > Date.now() && dates.sector === this.sectors[key].value;
+              });
+            }
+
+            // Sort dates from min to max
+            this.sortedDates = this.sortedDates.sort((a, b) =>  a.term - b.term);
+            this.nextDate = this.sortedDates[0].term;
+            this.type = this.sortedDates[0].type;
+          }
+        }
+      }
     });
 
     this.dataService.sectorUpdated.subscribe(
       (id: number) => {
-        this.descriptionTitle = this.dataService.getSectors()[id].name;
-        this.descriptionBoundary = this.dataService.getSectors()[id].boundary;
+        this.descriptionTitle = this.sectors[id].name;
+        this.descriptionBoundary = this.sectors[id].boundary;
 
         // Get nearest garbage collection date
         if (id === 0) {
@@ -47,7 +76,7 @@ export class DescriptionComponent implements OnInit {
             if (dates.sector === 'yellow') {
               dates.sector = 'gold';
             }
-            return +dates.term > Date.now() && dates.sector === this.dataService.getSectors()[id].value;
+            return +dates.term > Date.now() && dates.sector === this.sectors[id].value;
           });
         }
 
