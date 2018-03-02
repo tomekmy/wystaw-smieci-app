76be8e0819978780eac0f8ce0f60d71c1f183960
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2018-03-02 12:46:25 +0100

    Add dynamic aria-label property

--- src/app/sectors/sectors.component.ts
+++ src/app/sectors/sectors.component.ts
@@ -10,6 +10,7 @@ export class SectorsComponent implements OnInit {
   // Default user sector is "Wszystkie" but user selection is stored in local storage
   userSector = localStorage.lastSelectedSector || 'inherit';
   sectors: {value: string, viewValue: string, name: string, boundary: string}[] = [];
+  sectorLabel: string;
 
   constructor(private dataService: DataService) { }
 
@@ -19,12 +20,16 @@ export class SectorsComponent implements OnInit {
     selectElementClass.setAttribute('style', 'color:' + select.value);
     // Get selected item id from $event object and use replace to change each letter to empty char - leave only number
     const id: any = select.source.selected.id.replace( /^\D+/g, '');
+    // Update aria-label on sector change
+    this.sectorLabel = this.sectors.filter(dates => dates.value === select.value)[0].viewValue;
+    // Emit sectorUpdated
     this.dataService.sectorUpdated.emit(+id);
   }
 
   ngOnInit() {
     this.sectors = this.dataService.getSectors();
     document.querySelector('.mat-select-value').setAttribute('style', 'color:' + this.userSector);
+    this.sectorLabel = this.sectors.filter(dates => dates.value === this.userSector)[0].viewValue;
   }
 
 }
